d0e35463ed0caa4d89560b0f6219422f17db8a9a
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-07-01 17:35:07 +0200

    Added basic project structure

---
+++ src/main/java/com/modwin/ModwinChatApp/ModwinChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.modwin.ModwinChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModwinChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModwinChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modwin/ModwinChatApp/ModwinChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modwin.ModwinChatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModwinChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
